53c61c83199673474444be7553cf22bc89675491
Jacob Sniff <jacobsniff18@gmail.com>
2023-06-16 17:11:57 -0400

    added 2 handlbars for main and home. and also included some simple css. NEEDS debuggin: when starting server and navigating to localhost 3001 the page does not render

--- server.js
+++ server.js
@@ -31,6 +31,10 @@ const sess = {
   
   app.engine('handlebars', hbs.engine);
   app.set('view engine', 'handlebars');
+
+  app.get('/', (req, res) => {
+    res.render('main', { layout: false });
+  });
   
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
